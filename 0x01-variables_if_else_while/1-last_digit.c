287812d55212a82bd1f559a4091f557592e28679
“Charles <charlznwoko@yahoo.com>
2023-03-21 12:29:19 -0700

    variables else and while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line
- * Return: Always 0 (Success)
+ *main - Entry point
+ *Description: 'get the last digit of a number'
+ *Return: always 0
  */
 int main(void)
 {
-	char ch;
+	int n;
+	int ld;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
+	if (ld > 5)
 	{
-		putchar(ch);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	putchar('\n');
+	else if (ld == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, ld);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
+	}
 	return (0);
 }
