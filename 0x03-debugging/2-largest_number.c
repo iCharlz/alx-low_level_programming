fddb6a2fe809b3aa7a0cedb8bf93f747455b4242
“Charles <charlznwoko@yahoo.com>
2023-03-23 15:41:39 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,20 +14,20 @@ int largest_number(int a, int b, int c)
 
  if (a > b && a > c)
  {
- largest = a;
+	 largest = a;
  }
  else if (a > b && c > a)
  {
- largest = c;
+	 largest = c;
  }
  else if (b > c)
  {
- largest = b;
+	 largest = b;
  }
  else
  {
- largest = c;
+	 largest = c;
  }
-
-return (largest);
+ 
+ return (largest);
 }
