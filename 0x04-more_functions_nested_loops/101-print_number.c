ce0f44f763eb6e36fd82315b6a270281691c6e83
“Charles <charlznwoko@yahoo.com>
2023-03-24 17:31:55 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,34 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
-*/
-void print_number(int n)
-{
-	unsigned int n1;
+ * print_triangle - entry point
+ * owned by Bwave/Bright Daniel
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
+ */
 
-	if (n < 0)
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
 	{
-		n1 = -n;
-		_putchar('-');
+		_putchar('\n');
 	}
 	else
 	{
-		n1 = n;
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0')
 }
