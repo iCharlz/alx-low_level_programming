40a8656e4001615d3258de6c07181494f82f1e7d
“Charles <charlznwoko@yahoo.com>
2023-05-01 13:34:13 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * listint_len - returns all the num of elements linked in the lists
+* listint_len - returns all the num of elements linked in the lists
  * @h: linked list of type listint_t to traverse
  *
  * Return: num of nodes
- */
+*/
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * add_nodeint - add a new node at the beginning of the linked list
+* add_nodeint - add a new node at the beginning of the linked list
  * @head: Point to the first node in the list
  * @n: insert data to the new node
  *
- * Return: point to the new node, or NULL if it fails
- */
+* Return: point to the new node, or NULL if it fails
+*/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
