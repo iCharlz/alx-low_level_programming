fc60c65ae3ece4e11b7151f272c832eeb238dd05
“Charles <charlznwoko@yahoo.com>
2023-05-04 15:25:03 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* binary_to_uint - convert binary num to unsigned int
+* @b: string containing the binary num
+*
+* Return: Converted num
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
+
+	return (dec_val);
+}
