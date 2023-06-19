06342361c7ebeb62323fe0902c3919513735f72d
“Charles <charlznwoko@yahoo.com>
2023-06-19 06:01:37 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,14 +10,12 @@
 int _strlen(char *s)
 {
 	int i;
-	
+
 	i = 0;
-	
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	
 	return (i);
 }
 
@@ -33,21 +31,18 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
-	
+
 	len = 0;
-	
 	while (src[len] != '\0')
 	{
 		len++;
 	}
-	
+
 	for (i = 0; i < len; i++)
 	{
-		
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-	
 	return (dest);
 }
 
@@ -63,14 +58,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int len1, len2;
-	
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-	
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
@@ -87,6 +81,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 		_strcpy(dog->name, name);
 		_strcpy(dog->owner, owner);
 		dog->age = age;
-		
 		return (dog);
 }
