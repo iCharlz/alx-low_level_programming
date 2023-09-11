20a26fbd757048c8af30c21a4cd988f4f781f08b
“Charles <charlznwoko@yahoo.com>
2023-09-11 14:22:30 -0700

    0x1A. C - Hash tables

--- 0x1A-hash_tables/1-main.c
+++ 0x1A-hash_tables/1-main.c
@@ -1,17 +1,3 @@
-unsigned long int hash_djb2(const unsigned char *str)
-{
-    unsigned long int hash;
-    int c;
-
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
-}
-julien@ubuntu:~/0x1A. Hash tables$ 
-julien@ubuntu:~/0x1A. Hash tables$ cat 1-main.c 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
